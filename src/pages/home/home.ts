b156241fca5f1652ac105d7ae4b9d8a73159f211
sinthuC <sinthu_ch@kkumail.com>
2018-06-29 13:52:21 +0700

    ADD Event subscribe to home

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,23 @@
 import { Component } from '@angular/core';
-import { NavController,ViewController, ModalController } from 'ionic-angular';
+import { NavController, Events , ModalController } from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+ sn1 :string;
+  constructor(public navCtrl: NavController,private modal : ModalController,private events:Events) {
+    this.sn1 = 'page1'
 
-  constructor(public navCtrl: NavController,private modal : ModalController) {
+    this.events.subscribe("sec1Submitted",() => {
+      this.sn1="page2"
+      
+  })
 
+  this.events.subscribe("sec2Submitted",() => {
+    this.sn1="page3"
+})
   }
   openconvert(){
     const myModal = this.modal.create('ConvertModalPage');
