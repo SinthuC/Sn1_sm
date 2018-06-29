e08eb8940d303bada2a34973f1a793bd3a4de309
sinthuC <sinthu_ch@kkumail.com>
2018-06-29 12:43:11 +0700

    Add IonicModule to components.modules

--- src/components/components.module.ts
+++ src/components/components.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { Sn1Sec1Component } from './sn1-sec1/sn1-sec1';
 import { Sn1Sec2Component } from './sn1-sec2/sn1-sec2';
 import { Sn1Sec3Component } from './sn1-sec3/sn1-sec3';
+import { IonicModule } from 'ionic-angular';
 @NgModule({
 	declarations: [Sn1Sec1Component,
     Sn1Sec2Component,
     Sn1Sec3Component],
-	imports: [],
+	imports: [IonicModule],
 	exports: [Sn1Sec1Component,
     Sn1Sec2Component,
     Sn1Sec3Component]
